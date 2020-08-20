85ad993fdf62d8cb4b87cc435416ac76f2e7e004
Boye A <boyeadeotan@gmail.com>
2020-08-20 09:05:34 +0100

    JAvascript functionality sorted

--- app.js
+++ app.js
@@ -1,16 +1,53 @@
-let run = document.getElementById("run").value;
 
-console.log(run);
-let add = document.getElementById("add").value;
-let total = document.getElementById("total").value;
+let add = document.getElementById("add");
 let avgDistance = document.getElementById("avgDistance").value;
-let weekHigh = document.getElementById("weekHigh").value;
+let weekDays = document.querySelector("#km-boxes-container");
 let runArray = [];
+let kmTotal = 0;
 
-const addition =(run)=>{
-    runArray.push(run);
+
+
+// Function to update individual list boxes with daily run
+const addition =()=>{
+    let runValue = document.getElementById("run").value;
+    if (runValue === 0){
+        return;
+    }else{
+    runArray.push(runValue);
+    let li = document.createElement("li");
+    li.appendChild(document.createTextNode(runValue));
+    let ulList = document.getElementById("km-boxes-container").firstElementChild;
+    weekDays.appendChild(li);
+    ulList.remove();
+    totaldistance();       
+}
 
 }
+
+//Function to compute and update total distance run
+
+function totaldistance(){
+    
+    kmTotal += Number(runArray.slice(-1)[0]);
+    document.getElementById("total").innerHTML = kmTotal.toFixed(1);
+    averagedist();
+}
+
+
+//Function to compute and update average distance
+
+function averagedist(){
+    let total = document.getElementById("total").innerHTML;
+    let average = Number(total)/runArray.length
+    document.getElementById("avgDistance").innerHTML = average.toFixed(1);
+
+    highest();
+}
+
+function highest(){;
+    document.getElementById("weekHigh").innerHTML = Math.max(...runArray)
+}
+
 
 
 
